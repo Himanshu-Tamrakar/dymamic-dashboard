1a65f71581b2bdf488c94355d5521db636779949
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-09-13 16:27:17 +0530

    workable row exchange

--- src/app/angular-cdk/drag-rop/drag-rop.component.ts
+++ src/app/angular-cdk/drag-rop/drag-rop.component.ts
@@ -13,50 +13,22 @@ export class DragRopComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
- //  timePeriods = [
- //   'Bronze age',
- //   'Iron age',
- //   'Middle ages',
- //   'Early modern period',
- //   'Long nineteenth century'
- // ];
- //
- // drop(event: CdkDragDrop<string[]>) {
- //   moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
- // }
+  ngOnInit() { }
 
- todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
 
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
-
-  prodDone = [
-    'prod-Get up',
-    'prod-Brush teeth',
-    'prod-Take a shower',
-    'prod-Check e-mail',
-    'prod-Walk dog'
+  timePeriods = [
+    'Google',
+    'Facebook',
+    'Apple',
+    'Micro Soft',
+    'Slack',
+    'Air bus',
+    'Service Now',
+    'ISRO'
   ];
 
   drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-    }
-
-    console.log(this.todo, this.done, this.prodDone);
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
   }
 
 }
