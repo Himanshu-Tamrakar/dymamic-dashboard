feb6087697acf6496b3e7e10bf09722217952f29
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-09-18 11:01:55 +0530

    workable canvas

--- src/app/designer/component/canvas/canvas.component.ts
+++ src/app/designer/component/canvas/canvas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDropList, CdkDragDrop, copyArrayItem } from '@angular/cdk/drag-drop';
+import { CdkDropList, CdkDragDrop, copyArrayItem, transferArrayItem, moveItemInArray } from '@angular/cdk/drag-drop';
 import { DropZoneService } from '../../service/drop-zone.service'
 import { Item } from '../../interface/item';
 
@@ -9,44 +9,6 @@ import { Item } from '../../interface/item';
   styleUrls: ['./canvas.component.scss']
 })
 export class CanvasComponent implements OnInit {
-  todo = [
-     'Google',
-     'Facebook',
-     'ISRO',
-     'Apple',
-     // 'Get to work1',
-     // 'Pick up groceries1',
-     // 'Go home1',
-     // 'Fall asleep1',
-     // 'Get to work2',
-     // 'Pick up groceries2',
-     // 'Go home2',
-     // 'Fall asleep2',
-     // 'Get to work3',
-     // 'Pick up groceries3',
-     // 'Go home3',
-     // 'Fall asleep3',
-     // 'Get to work4',
-     // 'Pick up groceries4',
-     // 'Go home4',
-     // 'Fall asleep4',
-   ];
-
-   done = [
-     'Get up',
-     'Brush teeth',
-     'Take a shower',
-     'Check e-mail',
-     // 'Walk dog'
-   ];
-
-   prodDone = [
-     'prod-Get up',
-     'prod-Brush teeth',
-     'prod-Take a shower',
-     'prod-Check e-mail',
-     // 'prod-Walk dog'
-   ];
 
   canvasItems: Item[] = [];
 
@@ -55,36 +17,42 @@ export class CanvasComponent implements OnInit {
   ngOnInit() {
   }
 
-  // drop(event: CdkDragDrop<any[]>) {
-  //
-  //   // if drop event is from an item that was already on canvas
-  //   if (event.container === event.previousContainer) {
-  //     // sort it based on where it was dropped
-  //     this.dropZones.moveInList(event);
-  //   } else {
-  //     // else the event was from a dropped palette item so add it to the list
-  //     copyArrayItem(event.previousContainer.data, event.container.data, 0, event.currentIndex);
-  //     this.decrementCount(event.previousContainer);
-  //     // console.log({event});
-  //   }
-  //
-  //   console.log()
-  //
-  // }
+   todo = [
+      'Google',
+      'Facebook',
+      'ISRO',
+      'Apple',
+    ];
 
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      console.log(event.previousContainer, event.container)
-      debugger
-      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    done = [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      // 'Walk dog'
+    ];
+
+    prodDone = [
+      'prod-Get up',
+      'prod-Brush teeth',
+      'prod-Take a shower',
+      'prod-Check e-mail',
+      // 'prod-Walk dog'
+    ];
+
+
+    sideOption = ['H', 'H', 'S', 'Y']
+
+    drop(event: CdkDragDrop<string[]>) {
+      console.log(event)
+      if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      } else {
+        console.log(event.previousContainer, event.container)
+
+        transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+      }
+      console.log(this.todo, this.done, this.prodDone)
     }
-  }
-
-  decrementCount(previousContainer) {
-    previousContainer.data[0].count--;
-    console.log(previousContainer.data);
-  }
 
 }
